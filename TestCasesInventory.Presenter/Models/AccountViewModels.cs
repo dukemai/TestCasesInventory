25ce239971f4703cc3637e0acf8cc599535aea06
hngbv95 <hngbv95@gmail.com>
2016-07-14 15:03:10 +0700

    Hung Bui : Add DisplayName Property for AccountViewModels.

--- TestCasesInventory.Presenter/Models/AccountViewModels.cs
+++ TestCasesInventory.Presenter/Models/AccountViewModels.cs
@@ -79,6 +79,11 @@ namespace TestCasesInventory.Presenter.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
+        [Display(Name = "Your DisplayName")]
+        public string DisplayName { get; set; }
     }
 
     public class ResetPasswordViewModel : ViewModelBase
