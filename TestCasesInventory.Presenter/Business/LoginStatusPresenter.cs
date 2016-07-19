2e8acda4e5290f0946da48da44f63e819d055ee2
haiuet <haiboytbvn@gmail.com>
2016-07-19 14:10:13 +0700

    add logic for DisplayName

---
+++ TestCasesInventory.Presenter/Business/LoginStatusPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCasesInventory.Data.DataModels;
+using TestCasesInventory.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TestCasesInventory.Presenter.Models;
+
+namespace TestCasesInventory.Presenter.Business
+{
+    public class LoginStatusPresenter : ILoginStatusPresenter
+    {
+        public LoginStatusViewModel GetCurrentUser(string email)
+        {
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var currentUser = manager.FindByEmail(email);
+            var viewModel = new LoginStatusViewModel { DisplayName = currentUser.DisplayName };
+            return viewModel;
+        }
+
+
+        //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+        //var currentUser = manager.FindById(User.Identity.GetUserId());
+
+
+    }
+}
---
+++ TestCasesInventory/Controllers/LoginStatusController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestCasesInventory.Presenter.Models;
+using TestCasesInventory.Presenter.Business;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+
+namespace TestCasesInventory.Controllers
+{
+
+    public class LoginStatusController : Controller
+    {
+
+        public ILoginStatusPresenter LoginStatusPresenter;
+
+        public LoginStatusController()
+        {
+            LoginStatusPresenter = new LoginStatusPresenter();
+        }
+        // GET: LoginStatus
+        //[ChildActionOnly]
+        [HttpGet]
+        public ActionResult DisplayName()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var model = LoginStatusPresenter.GetCurrentUser(User.Identity.GetUserName());
+                return PartialView("~/Views/Shared/_AuthenticatedPartial.cshtml", model);
+            }
+            else
+            {
+                return PartialView("~/Views/Shared/_UnAuthenticatedPartial.cshtml");
+            }
+        }
+    }
+}
