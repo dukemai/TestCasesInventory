1e2eba05fcc1de901e331bf1b4c0a7454a8a6922
hngbv95 <hngbv95@gmail.com>
2016-08-02 17:02:46 +0700

    Hung Bui: Develop TestCaseRepository

---
+++ TestCasesInventory.Data/Repositories/ITestCaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TestCasesInventory.Data.DataModels;
+
+
+namespace TestCasesInventory.Data.Repositories
+{
+    public interface ITestCaseRepository
+    {
+        IEnumerable<TestCaseDataModel> ListAll();
+        TestCaseDataModel GetTestCaseByID(int testCaseID);
+        void InsertTestCase(TestCaseDataModel testCase);
+        void DeleteTestCase(int testCaseID);
+        void UpdateTestCase(TestCaseDataModel testCase);
+        void Save();
+    }
+}
---
+++ TestCasesInventory.Data/Repositories/TestCaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TestCasesInventory.Data.DataModels;
+
+namespace TestCasesInventory.Data.Repositories
+{
+    public class TestCaseRepository : RepositoryBase, ITestCaseRepository
+    {
+        public TestCaseRepository() : base(){ }
+
+        public IEnumerable<TestCaseDataModel> ListAll()
+        {
+            return dataContext.TestCases.ToList();
+        }
+
+        public TestCaseDataModel GetTestCaseByID(int testCaseID)
+        {
+            return dataContext.TestCases.Find(testCaseID);
+        }
+
+        public void InsertTestCase(TestCaseDataModel testCase)
+        {
+            dataContext.TestCases.Add(testCase);
+        }
+
+
+        public void DeleteTestCase(int testCaseID)
+        {
+            var TestCaseDataModel = dataContext.TestCases.Find(testCaseID);
+            dataContext.TestCases.Remove(TestCaseDataModel);
+        }
+
+        public void UpdateTestCase(TestCaseDataModel testCase)
+        {
+            dataContext.Entry(testCase).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            dataContext.SaveChanges();
+        }
+    }
+}
