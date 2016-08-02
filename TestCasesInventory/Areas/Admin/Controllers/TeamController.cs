72f904437bab79071e61c22900c9192a30759fe0
hngbv95 <hngbv95@gmail.com>
2016-08-02 13:49:51 +0700

    Hung Bui : Add DisplayName and CurrentTeam in AssignUsersToTeam View

--- TestCasesInventory/Areas/Admin/Controllers/TeamController.cs
+++ TestCasesInventory/Areas/Admin/Controllers/TeamController.cs
@@ -168,7 +168,7 @@ namespace TestCasesInventory.Areas.Admin.Controllers
         {
             try
             {
-                ViewBag.id = id;
+                ViewBag.ID = id;
                 var team = TeamPresenterObject.GetTeamById(id);
                 var listUsersNotBelongTeam = TeamPresenterObject.ListUsersNotBelongTeam(id);
                 return View("AddUsersToTeam", listUsersNotBelongTeam);
@@ -204,7 +204,7 @@ namespace TestCasesInventory.Areas.Admin.Controllers
         {
             try
             {
-                ViewBag.id = id;
+                ViewBag.ID = id;
                 var team = TeamPresenterObject.GetTeamById(id);
                 var listUsersBelongTeam = TeamPresenterObject.ListUsersBelongTeam(id);
                 return View("RemoveUsersFromTeam", listUsersBelongTeam);
