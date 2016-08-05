734fa49127093530d5afb4e57d1841b29ebc48a3
duc.mai <duc.mai@niteco.se>
2016-08-05 10:46:45 +0700

    refactor Upload profile

---
+++ TestCasesInventory.Web.Common/Utils/PathHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace TestCasesInventory.Web.Common.Utils
+{
+    public class PathHelper
+    {
+        public static void EnsureDirectories(string filePath)
+        {
+            var dirs = Path.GetDirectoryName(filePath);
+
+            if (!Directory.Exists(dirs))
+            {
+                Directory.CreateDirectory(dirs);
+            }
+        }
+    }
+}
