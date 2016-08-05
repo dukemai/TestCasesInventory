337adcd22090715337c53dfa086a52fb6b202b6a
haiuet <haiboytbvn@gmail.com>
2016-08-05 15:17:54 +0700

    Hai Bui: Menu for privileged user

---
+++ TestCasesInventory/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestCasesInventory.Web.Common;
+
+namespace TestCasesInventory.Controllers
+{
+    public class MenuController : Controller
+    {
+        protected PrivilegedUsersConfig privilegedUsers;
+
+        public MenuController()
+        {
+            privilegedUsers = new PrivilegedUsersConfig();
+        }
+
+
+        // GET: Menu
+        public ActionResult Index()
+        {
+            if (User.IsInRole(privilegedUsers.AdminRole))
+            {
+                return PartialView("~/Views/Shared/_AdminPartial.cshtml");
+            }
+
+            if (User.IsInRole(privilegedUsers.TesterRole))
+            {
+                return PartialView("~/Views/Shared/_TesterPartial.cshtml");
+            }
+
+            return PartialView("~/Views/Shared/_UnprivilegedPartial.cshtml");
+        }
+    }
+}
