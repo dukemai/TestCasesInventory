329f847838887708b36bf35bbe90d90bf77fb0b5
hngbv95 <hngbv95@gmail.com>
2016-08-08 13:35:23 +0700

    Hung Bui : Add tinymce to TestCase and TestSuite TextArea

--- TestCasesInventory.Data/DataModels/TestSuiteDataModel.cs
+++ TestCasesInventory.Data/DataModels/TestSuiteDataModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace TestCasesInventory.Data.DataModels
 {
@@ -8,6 +9,7 @@ namespace TestCasesInventory.Data.DataModels
     {
         [Required]
         public string Title { get; set; }
+        [AllowHtml]
         public string Description { get; set; }
         public string Created { get; set; }
         public string LastModified { get; set; }
---
+++ TestCasesInventory/Scripts/tinymceInit.js
@@ -0,0 +1,20 @@
+﻿tinymce.init({
+    selector: 'textarea',
+    height: 100,
+    width: '85%',
+    theme: 'modern',
+    plugins: [
+      'advlist autolink link image preview hr',
+      'paste textcolor colorpicker imagetools'
+    ],
+    toolbar1: 'insertfile undo redo | styleselect | bold italic underline| alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview media | forecolor backcolor',
+    image_advtab: true,
+    templates: [
+      { title: 'Test template 1', content: 'Test 1' },
+      { title: 'Test template 2', content: 'Test 2' }
+    ],
+    content_css: [
+      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+      '//www.tinymce.com/css/codepen.min.css'
+    ]
+});
