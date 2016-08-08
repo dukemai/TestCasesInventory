a67ec92f56c49a2a50c341419a902f735de0640e
haiuet <haiboytbvn@gmail.com>
2016-08-08 14:32:10 +0700

    Hai Bui: fix in UI
    	- Remove . in Manager.
    	- Remove Name in Team Name
    	- Manage TestSuites -> Manage Test Suites
    	- Add btn-default to Create New

--- TestCasesInventory.Presenter/Models/ManageViewModels.cs
+++ TestCasesInventory.Presenter/Models/ManageViewModels.cs
@@ -13,6 +13,7 @@ namespace TestCasesInventory.Presenter.Models
         public string Email { get; set; }
         [Display(Name = "Name")]
         public string DisplayName { get; set; }
+        [Display(Name = "Team")]
         public string TeamName { get; set; }
         public bool HasPassword { get; set; }
         [Display(Name = "Role")]
