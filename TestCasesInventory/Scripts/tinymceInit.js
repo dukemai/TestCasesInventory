75a9e98ed5f92c47af149af7ac0f24dbe15b3e80
hngbv95 <hngbv95@gmail.com>
2016-08-08 14:28:37 +0700

    Hung Bui : delete test templates

--- TestCasesInventory/Scripts/tinymceInit.js
+++ TestCasesInventory/Scripts/tinymceInit.js
@@ -1,6 +1,6 @@
 ﻿tinymce.init({
     selector: 'textarea',
-    height: 100,
+    height: '100',
     width: '85%',
     theme: 'modern',
     plugins: [
@@ -9,10 +9,6 @@
     ],
     toolbar1: 'insertfile undo redo | styleselect | bold italic underline| alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview media | forecolor backcolor',
     image_advtab: true,
-    templates: [
-      { title: 'Test template 1', content: 'Test 1' },
-      { title: 'Test template 2', content: 'Test 2' }
-    ],
     content_css: [
       '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
       '//www.tinymce.com/css/codepen.min.css'
