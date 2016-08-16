a2354079cdc0d077297f853dd088bf5d4a1ba1ef
duc.mai <duc.mai@niteco.se>
2016-08-16 13:54:00 +0700

    add MapperExtensions

---
+++ TestCasesInventory.Presenter.Common/MapperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using PagedList;
+
+namespace TestCasesInventory.Presenter.Common
+{
+    public static class MapperExtensions
+    {
+        public static Destination MapTo<Source,Destination>(this Source source)
+        {
+            return Mapper.Map<Destination>(source);
+        }
+    }
+}
