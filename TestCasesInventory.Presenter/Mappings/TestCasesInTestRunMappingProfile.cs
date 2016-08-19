1f50591631a04e70277bcd2aed525b305d0f79a8
Hanh Pham <Hanh Pham>
2016-08-19 17:38:51 +0700

    Hanh Pham: Use Mapper for TestCasesInTestRun

---
+++ TestCasesInventory.Presenter/Mappings/TestCasesInTestRunMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PagedList;
+using TestCasesInventory.Data.DataModels;
+using TestCasesInventory.Presenter.Models;
+
+namespace TestCasesInventory.Presenter.Mappings
+{
+    public class TestCasesInTestRunMappingProfile : Profile
+    {
+        public TestCasesInTestRunMappingProfile(string profileName) : base(profileName)
+        {
+            this.CreateMap<TestCasesInTestRunDataModel, TestCasesInTestRunViewModel>();
+                    
+            this.CreateMap<IPagedList<TestCasesInTestRunDataModel>, IPagedList<TestCasesInTestRunViewModel>>()
+                .ConvertUsing<Mappings.PagedListConverter<TestCasesInTestRunDataModel, TestCasesInTestRunViewModel>>();
+            
+        }
+
+    }
+}
---
+++ TestCasesInventory.Presenter/Mappings/TestRunMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TestCasesInventory.Presenter.Mappings
+{
+    public class TestRunMappingProfile: Profile
+    {
+        public TestRunMappingProfile(string profileName) : base(profileName)
+        {
+
+        }
+    }
+}
