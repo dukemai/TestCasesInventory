776c64d2573e8330400b1317fdafba2ed4ea5ae4
duc.mai <duc.mai@niteco.se>
2016-08-22 18:05:21 +0700

    test suite mapping

--- TestCasesInventory.Presenter/Mappings/TestSuiteMappingProfile.cs
+++ TestCasesInventory.Presenter/Mappings/TestSuiteMappingProfile.cs
@@ -12,27 +12,36 @@ namespace TestCasesInventory.Presenter.Mappings
     public class TestSuiteMappingProfile : Profile
     {
         ITestCaseRepository testCaseRepository;
-        UserManager<ApplicationUser> UserManager;
         TeamRepository teamRepository;
 
         public TestSuiteMappingProfile(string profileName)
             : base(profileName)
         {
             testCaseRepository = new TestCaseRepository();
-
-            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            teamRepository = new TeamRepository();
 
             this.CreateMap<TestSuiteDataModel, TestSuiteViewModel>()
                  .ForMember(dest => dest.TestCasesNumber, opt => opt.MapFrom(src => testCaseRepository.TotalTestCasesForTestSuite(src.ID)))
-                 .ForMember(dest => dest.Created, opt => opt.MapFrom(src => UserManager.FindByEmail(src.Created).DisplayName))
-                 .ForMember(dest => dest.LastModified, opt => opt.MapFrom(src => UserManager.FindByEmail(src.LastModified).DisplayName));
+                 .ForMember(dest => dest.CreateDisplayOnly, opt =>
+                 {
+                     opt.MapFrom(src => string.IsNullOrEmpty(src.CreateDisplayOnly) ? teamRepository.FindUserByEmail(src.Created).DisplayName: src.CreateDisplayOnly);
+                 })
+                 .ForMember(dest => dest.LastModifiedDisplayOnly, opt =>
+                 {
+                     opt.MapFrom(src => string.IsNullOrEmpty(src.LastModifiedDisplayOnly) ? teamRepository.FindUserByEmail(src.LastModifiedDisplayOnly).DisplayName : src.LastModifiedDisplayOnly);
+                 })
+                 .ForMember(dest => dest.TeamNameDisplayOnly, opt =>
+                 {
+                     opt.MapFrom(src => string.IsNullOrEmpty(src.TeamNameDisplayOnly) ? teamRepository.GetTeamByID(src.TeamID).Name : src.TeamNameDisplayOnly);
+                 });
             this.CreateMap<IPagedList<TestSuiteDataModel>, IPagedList<TestSuiteViewModel>>()
                 .ConvertUsing<Mappings.PagedListConverter<TestSuiteDataModel, TestSuiteViewModel>>();
 
             this.CreateMap<CreateTestSuiteViewModel, TestSuiteDataModel>()
-                .ForMember(dest => dest.TeamID, opt => opt.MapFrom(src => UserManager.FindByEmail(src.Created).TeamID));
+                .ForMember(dest => dest.TeamID, opt => opt.MapFrom(src => teamRepository.FindUserByEmail(src.Created).TeamID));
 
             this.CreateMap<EditTestSuiteViewModel, TestSuiteDataModel>();
+
         }
     }
 }
