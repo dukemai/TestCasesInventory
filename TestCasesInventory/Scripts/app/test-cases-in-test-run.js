ce80622a7071a6fdf99f685724975c4655f8fe3e
hngbv95 <hngbv95@gmail.com>
2016-08-25 15:44:39 +0700

    Hung Bui : Get TestCases Json Object.

--- TestCasesInventory/Scripts/app/test-cases-in-test-run.js
+++ TestCasesInventory/Scripts/app/test-cases-in-test-run.js
@@ -4,9 +4,17 @@ $('.modal-link').click(function (e) {
     e.preventDefault();
     var thisHref = this.href;
     $.get(thisHref, function (data, status) {
+        var testRunID = data[1].TestRunID;
         var result = template(data);
         $("#modalContent").empty();
         $("#modalContent").append(result);
         $('#modal-container').modal('show');
+        $("#modalContent a").click(function (e) {
+            var testSuiteID = $(this).attr("data-id");
+            $.get("TestRun/GetTestCasesInTestSuitePopUp?testSuiteID=" + testSuiteID + "&testRunID=" + testRunID, function (data, status) {
+                console.log(data);
+            })
+        })
     });
 })
+
