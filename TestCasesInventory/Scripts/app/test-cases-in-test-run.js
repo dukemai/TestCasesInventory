9d96ec8be0b25e70717131a2b97e4de1f692c5e2
hngbv95 <hngbv95@gmail.com>
2016-08-26 12:18:23 +0700

    Hung Bui : Create Templates folder for store handlebars template htm

--- TestCasesInventory/Scripts/app/test-cases-in-test-run.js
+++ TestCasesInventory/Scripts/app/test-cases-in-test-run.js
@@ -1,20 +1,35 @@
-﻿
+﻿var templates = {};
 
-$('.modal-link').click(function (e) {
+function return_html(appendTo, tmpl, data) {
+
+    if (templates[tmpl] === undefined) {
+        jQuery.get("/Templates/handlebars_template_" + tmpl + ".htm", function (resp) {
+            templates[tmpl] = Handlebars.compile(resp);
+            return_html(appendTo, tmpl, data);
+        });
+        return;
+    }
+    var template = templates[tmpl];
+    var html = template(data);
+    $(appendTo).empty();
+    $(appendTo).html(html);
+}
+
+$('.modal-link').on("click", function (e) {
     e.preventDefault();
     var thisHref = this.href;
     $.get(thisHref, function (data, status) {
-        var testRunID = data[1].TestRunID;
-        var result = template(data);
-        $("#modalContent").empty();
-        $("#modalContent").append(result);
+        var testRunID = data[0].TestRunID;
+        return_html("#modalContent", "test_run", data);
         $('#modal-container').modal('show');
-        $("#modalContent a").click(function (e) {
+        $("#modalContent").on("click","a" ,function () {
             var testSuiteID = $(this).attr("data-id");
             $.get("TestRun/GetTestCasesInTestSuitePopUp?testSuiteID=" + testSuiteID + "&testRunID=" + testRunID, function (data, status) {
                 console.log(data);
-            })
+            });
         })
     });
 })
+
+
 
