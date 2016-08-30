e452c9118417e68860e3aa0cb8f93955cbc3ed5f
squidthetracker <long.tran3@niteco.se>
2016-08-30 13:57:07 +0700

    Long Tran: Add some Mappings and View Models

--- TestCasesInventory.Presenter/StartUp.cs
+++ TestCasesInventory.Presenter/StartUp.cs
@@ -19,6 +19,7 @@ namespace TestCasesInventory.Presenter
                 cfg.AddProfile(new UserMappingProfile("UserMapping"));
                 cfg.AddProfile(new TestRunMappingProfile("TestRunMapping"));
                 cfg.AddProfile(new TestRunResultMappingProfile("TestRunResultMapping"));
+                cfg.AddProfile(new TestCasesInTestRunMappingProfile("TestCasesInTestRunMapping"));
 
             });
         }
