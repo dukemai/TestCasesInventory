734a59dda7f854e38418a3e80a3ddcce52455541
squidthetracker <long.tran3@niteco.se>
2016-09-09 11:31:50 +0700

    Long Tran: Render Chart with real Data.

--- TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/status-chart-view.js
+++ TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/status-chart-view.js
@@ -4,9 +4,78 @@
             this.model = data;
         }
 
+        statusChartView.prototype.initChart = function () {
+            var self = this;
+            var chartArea = $('#status-chart');
+            if (chartArea.length) {
+                var chart = {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    
+                    borderColor: "gray",
+                    //#90ed7d
+                    borderRadius: 1,
+                    borderWidth: 4
+
+                };
+                var title = {
+                    text: 'Test Run Results By Status Overview'
+                };
+                var subtitle = {
+                    text: self.model.TestRunTitle,
+                };
+                var credits = {
+                    href: "#",
+                    text: "NYT 2016"
+                }
+                var tooltip = {
+                    //pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    pointFormat: ' Number of {series.name}: <b>{point.y}</b>',
+                    hideDelay: 300
+                };
+                var plotOptions = {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name} </b>: {point.percentage:.2f} %',
+                            style: {
+                                color: 'black'
+                                //(Highcharts.theme && Highcharts.theme.contrastTextColor) || 
+                            }
+                        }
+                    }
+                };
+                var resultInfo =
+                     [
+                    { "name": "Skipped", "y": self.model.NumberOfSkippedTestCases },
+                    { "name": "Failed", "y": self.model.NumberOfFailedTestCases },
+                    { "name": "Passed", "y": self.model.NumberOfPassedTestCases }
+                     ]
+                var series = [{
+                    type: 'pie',
+                    name: 'Test Cases',
+                    data: resultInfo
+                }];
+
+                var chartInfo = {};
+                chartInfo.chart = chart;
+                chartInfo.title = title;
+                chartInfo.subtitle = subtitle;
+                chartInfo.credits = credits;
+                chartInfo.tooltip = tooltip;
+                chartInfo.series = series;
+                chartInfo.plotOptions = plotOptions;
+
+                chartArea.highcharts(chartInfo);
+            }
+        }
+
         statusChartView.prototype.render = function () {
             var self = this;
-            $('#status-chart') ;
+            self.initChart();
         }
 
         return statusChartView;
--- TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/tester-chart-view.js
+++ TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/tester-chart-view.js
@@ -3,10 +3,76 @@
         function testerChartView(data) {
             this.model = data;
         }
+        testerChartView.prototype.initChart = function () {
+            var self = this;
+            var chartArea = $('#tester-chart');
+            if (chartArea.length) {
+                var chart = {
+                    backgroundColor: "#f3f3f3",
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    borderColor: "gray",
+                    borderRadius: 1,
+                    borderWidth: 4
 
+                };
+                var title = {
+                    text: 'Test Run Results By Tester Overview'
+                };
+                var subtitle = {
+                    text: self.model.TestRunTitle 
+                };
+                var credits = {
+                    href: "#",
+                    text: "NYT 2016"
+                }
+                var tooltip = {
+                    //pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    pointFormat: ' Number of {series.name}: <b>{point.y}</b>',
+                    hideDelay: 300
+                };
+                var plotOptions = {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name} </b>: {point.percentage:.2f} %',
+                            style: {
+                                color: 'black'
+                                //(Highcharts.theme && Highcharts.theme.contrastTextColor) || 
+                            }
+                        }
+                    }
+                };
+                var resultInfo =
+                     [
+                    { "name": "User1", "y": 9 },
+                    { "name": "User2", "y": 7 },
+                    { "name": "Admin", "y": 25 }
+                     ]
+                var series = [{
+                    type: 'pie',
+                    name: 'Test Cases',
+                    data: resultInfo
+                }];
+
+                var chartInfo = {};
+                chartInfo.chart = chart;
+                chartInfo.title = title;
+                chartInfo.subtitle = subtitle;
+                chartInfo.credits = credits;
+                chartInfo.tooltip = tooltip;
+                chartInfo.series = series;
+                chartInfo.plotOptions = plotOptions;
+
+                chartArea.highcharts(chartInfo);
+            }
+        }
         testerChartView.prototype.render = function () {
             var self = this;
-            $('#tester-chart');
+            self.initChart();
         }
 
         return testerChartView;
