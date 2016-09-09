f195af0fde50489ffedf5f39ff8411fff2c70951
hngbv95 <hngbv95@gmail.com>
2016-09-09 10:03:41 +0700

    Hung Bui : Refractor Client Side Code for Test Run Result Detail

--- TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/status-chart-view.js
+++ TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/status-chart-view.js
@@ -1,19 +1,12 @@
-﻿define(['templateHelper','promise', 'underscore'],
-    function (templateHelper, promise, _) {
+﻿define(['promise', 'underscore', 'highCharts', 'exporting'],
+    function (promise, _) {
         function statusChartView(data) {
             this.model = data;
-            this.template = '';
         }
 
         statusChartView.prototype.render = function () {
             var self = this;
-            var promisedResult = templateHelper.loadAndCache('status-chart', '/ClientSide/Templates/TestRunResult/tesrun-result-status-chart.html');
-            if (promisedResult) {
-                promisedResult.then(function () {
-                    self.template = templateHelper.templates['status-chart'];
-                    $('#status-chart').append(self.template(self.model));
-                })
-            }
+            $('#status-chart') ;
         }
 
         return statusChartView;
--- TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/tester-chart-view.js
+++ TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Views/tester-chart-view.js
@@ -1,19 +1,12 @@
-﻿define(['templateHelper','promise', 'underscore'],
-    function (templateHelper, promise, _) {
+﻿define(['promise', 'underscore', 'highCharts', 'exporting'],
+    function (promise, _) {
         function testerChartView(data) {
             this.model = data;
-            this.template = '';
         }
 
         testerChartView.prototype.render = function () {
             var self = this;
-            var promisedResult = templateHelper.loadAndCache('tester-chart', '/ClientSide/Templates/TestRunResult/testrun-result-tester-chart.html');
-            if (promisedResult) {
-                promisedResult.then(function () {
-                    self.template = templateHelper.templates['tester-chart'];
-                    $('#tester-chart').append(self.template(self.model));
-                })
-            }
+            $('#tester-chart');
         }
 
         return testerChartView;
