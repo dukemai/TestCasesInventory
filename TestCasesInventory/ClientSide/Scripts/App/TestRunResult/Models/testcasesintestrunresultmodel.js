2be09d1defe14b0a2c6e8ef5a39442e3a722f8df
squidthetracker <long.tran3@niteco.se>
2016-09-13 09:59:35 +0700

    Long Tran: solve conflict

--- TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Models/testcasesintestrunresultmodel.js
+++ TestCasesInventory/ClientSide/Scripts/App/TestRunResult/Models/testcasesintestrunresultmodel.js
@@ -1,5 +1,5 @@
 ﻿define(['moment'], function (moment) {
-    function testCasesInTestRunResultModel(id, title, priority, decription, precondition, expect, status, comment, priorityStyleClass, statusStyleClass, RunBy, LastRunDate) {
+    function testCasesInTestRunResultModel(id, title, priority, decription, precondition, expect, status, comment, priorityStyleClass, statusStyleClass, RunBy, LastRunDate, TestRunTitle) {
         this.ID = id;
         this.TestCaseTitle = title;
         this.TestCasePriority = priority;
@@ -12,6 +12,7 @@
         this.StatusStyleClass = statusStyleClass;
         this.RunBy = status == "Skipped" ? null : RunBy;
         this.LastRunDate = this.RunBy == null ? "" : moment(new Date(parseInt(LastRunDate.substr(6)))).format('MM/DD/YYYY, h:mm:ss a');
+        this.TestRunTitle = TestRunTitle;
     }
     return testCasesInTestRunResultModel;
 });
