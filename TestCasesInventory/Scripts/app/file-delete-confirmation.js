06ae561d180bd43ec192ad17f3550d0e9d1fffe3
duc.mai <duc.mai@niteco.se>
2016-09-16 10:17:25 +0700

    remove scripts folder

--- TestCasesInventory/Scripts/app/file-delete-confirmation.js
+++
@@ -1,6 +0,0 @@
-﻿
-    $(document).ready(function () {
-        $(".delete-item").click(function () { // Click to only happen on deleteItem links
-            $("#item").val($(this).data('id'));
-        });
-    });
--- TestCasesInventory/Scripts/app/list-user-customjs.js
+++
@@ -1,6 +0,0 @@
-﻿
-
-//var selectedTab = localStorage.getItem('selectedTab');
-//if (selectedTab != null) {
-//    $('a[data-toggle="tab"][href="' + selectedTab + '"]').tab('show');
-//}
